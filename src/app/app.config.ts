fb914d67cac9edf90fddcc9162275853db31960e
mariavand <mariamasha.vandoulaki@gmail.com>
2025-05-14 21:08:12 +0300

    configured auth0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAuth0({
+      domain: 'dev-s0fcza4jc8t1ucnt.eu.auth0.com',
+      clientId: 'BDuYiN7T0Qvh1qRAeCJcWYJykZyCXNiI',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    })
+  ]
 };

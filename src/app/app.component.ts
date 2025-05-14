0f487685849ffca08155a82f0e25f284e462eaa3
mariavand <mariamasha.vandoulaki@gmail.com>
2025-05-14 21:07:06 +0300

    Standalone in 1 file app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { AuthService, User } from '@auth0/auth0-angular';
+import { first, map } from 'rxjs';
 
 @Component({
-  selector: 'wfm-root',
+  selector: 'atp-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  template: `
+    <button (click)="auth.loginWithRedirect()">Log in</button>
+    <router-outlet />
+  `,
+  styles: `
+
+  `
 })
 export class AppComponent {
-  title = 'atp-auto-track-pro-angular';
+  auth = inject(AuthService);
+
+  user: Signal<User | null | undefined> = toSignal(this.auth.user$.pipe(
+    takeUntilDestroyed(),
+    map((u) => {
+      console.log('in map', u);
+      return u;
+    })
+  ));
+
+  constructor(){
+    console.log('user', this.user);
+  }
 }

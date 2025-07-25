3c63fe0d25bd24351fbb211b2f3d1b1182206b7e
Maria Vandoulaki <mariamasha.vandoulaki@gmail.com>
2025-07-25 08:43:39 +0300

    Moved custom checkbox styles

--- src/app/shared/utilities/components/custom-checkbox.component.ts
+++ src/app/shared/utilities/components/custom-checkbox.component.ts
@@ -13,6 +13,64 @@ import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModu
     </div>
   `,
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+  styles: [`
+    .checkbox-wrapper-57 input[type="checkbox"]
+      visibility: hidden;
+      display: none;
+
+    .checkbox-wrapper-57 *,
+    .checkbox-wrapper-57 ::after,
+    .checkbox-wrapper-57 ::before
+      box-sizing: border-box;
+
+    .checkbox-wrapper-57 .checkbox-container
+      display: flex;
+      gap: 1.2rem;
+      color: #F3F3F3;
+      position: relative;
+      cursor: pointer;
+      font-size: 1.2rem;
+      user-select: none;
+
+    /* Create a custom checkbox */
+    .checkbox-wrapper-57 .checkmark
+      position: relative;
+      top: 0;
+      left: 0;
+      height: 1.3em;
+      width: 1.3em;
+      background: #F3F3F3;
+      border-radius: 50px;
+      transition: all 0.7s;
+      --spread: 1rem;
+
+    .checkbox-wrapper-57 .checkbox-container input:checked ~ .checkmark
+      background: #F3F3F3;
+      box-shadow: -5px -5px var(--spread) 0px #5B51D8, 0 -5px var(--spread) 0px #833AB4, 5px -5px var(--spread) 0px #E1306C, 5px 0 var(--spread) 0px #FD1D1D, 5px 5px var(--spread) 0px #F77737, 0 5px var(--spread) 0px #FCAF45, -5px 5px var(--spread) 0px #FFDC80;
+
+    /* Create the checkmark/indicator (hidden when not checked) */
+    .checkbox-wrapper-57 .checkmark::after
+      content: "";
+      position: absolute;
+      display: none;
+
+    /* Show the checkmark when checked */
+    .checkbox-wrapper-57 .checkbox-container input:checked ~ .checkmark::after
+      display: block;
+
+    /* Style the checkmark/indicator */
+    .checkbox-wrapper-57 .checkbox-container .checkmark::after
+      left: 0.5em;
+      top: 0.34em;
+      width: 0.25em;
+      height: 0.5em;
+      border: solid #303841;
+      border-width: 0 0.15em 0.15em 0;
+      transform: rotate(45deg);
+
+    .checkbox-wrapper-57
+      padding: 1rem 1.5rem;
+  `],
   imports: [FormsModule, ReactiveFormsModule],
   providers:[
     {

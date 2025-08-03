1422f7cc467416cfc7f56bb3a2d0c91092110d7a
Maria Vandoulaki <mariamasha.vandoulaki@gmail.com>
2025-08-03 09:32:02 +0300

    Deleted unecessary declarations and comments

--- src/app/shared/models/car.model.ts
+++ src/app/shared/models/car.model.ts
@@ -6,18 +6,4 @@ export type Car = AbstractCar & {
   readonly year: number;
 }
 
-export type EditableCarFields = Omit<Car, 'carId' | 'model' | 'brand' | 'year'>
-
 export type CarColumnKey = Exclude<keyof Car, 'lockedBy'>;
-
-// export type CarGeneral = Pick<Car, 'carId' | 'ownerNameSurname' | 'serialNumber' | 'paymentStatus' | 'status' | 'color' | 'generalComments'>;
-
-// export type CarGeneralKeys = keyof CarGeneral;
-
-// export type CarSales = Pick<Car, 'buyingDay' | 'initialPrice' | 'finalPrice' | 'paymentStatus' | 'salesComments'>;
-
-// export type CarSalesKeys = keyof CarSales;
-
-// export type CarTech = Pick<Car, 'softwareVersion' | 'batteryChangeDate' | 'airConditioning' | 'fuelType' | 'seats' | 'transmission' | 'gps'| 'bluetooth' | 'techComments'>;
-
-// export type CarTechKeys = keyof CarTech;
